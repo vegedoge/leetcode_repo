5eb85432e61a88855e9077bd066d6300c2ea585c
blakewu <yx.wu.work@gmail.com>
2024-02-16 00:02:15 +0800

    fix: problem 1046 finished

--- 1046.最后一块石头的重量.py
+++ 1046.最后一块石头的重量.py
@@ -10,6 +10,18 @@ class Solution:
         while len(stones) > 1:
             max_value = max(stones)
             max_index = stones.index(max_value)
+            del stones[max_index]
+            second_value = max(stones)
+            sec_index = stones.index(second_value)
+            del stones[sec_index]
+            remain = max_value - second_value
+            if remain:
+                stones.append(remain)
+        if len(stones) == 0:
+            return 0
+        else:
+            return stones[0]
+                
             
 # @lc code=end
 
