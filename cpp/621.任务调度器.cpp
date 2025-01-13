ab84f3166dfe4a52d5a250b692628e8de28341a8
Yuxuan Wu <Y.Wu@student.tudelft.nl>
2025-01-13 23:49:58 +0100

    task 621

---
+++ cpp/621.任务调度器.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=621 lang=cpp
+ *
+ * [621] 任务调度器
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector <int> vec(26, 0);
+        for (char c : tasks) {
+            vec[c - 'A']++;
+        }
+        sort(vec.begin(), vec.end(), greater<int>());
+        int cnt = 1;
+        // record those tasks cant be inserted into the interval
+        while (cnt < vec.size() && vec[cnt] == vec[0]) {
+            cnt++;
+        }
+        return max((vec[0] - 1) * (n + 1) + cnt, (int)tasks.size());
+    }
+};
+// @lc code=end
+
