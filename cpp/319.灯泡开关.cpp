a8c709e8e78f9c3342d715dd004197534b0e01fc
Yuxuan Wu <Y.Wu@student.tudelft.nl>
2025-03-25 23:40:28 +0100

    练习

---
+++ cpp/319.灯泡开关.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=319 lang=cpp
+ *
+ * [319] 灯泡开关
+ */
+
+#include <math.h>
+
+// @lc code=start
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return static_cast<int>(sqrt(n));
+    }
+};
+// @lc code=end
+
