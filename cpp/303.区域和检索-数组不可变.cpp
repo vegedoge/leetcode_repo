b6ee0d81d5843170ec9b1867935d109ad92013d4
Yuxuan <yx.wu.work@gmail.com>
2025-08-07 16:07:39 +0800

    0807 daily pre-sum

---
+++ cpp/303.区域和检索-数组不可变.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=cpp
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+class NumArray {
+private:
+    vector<int> prefix;
+
+public:
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size() + 1);
+        prefix[0] = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            prefix[i + 1] = nums[i] + prefix[i];
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+// @lc code=end
+
